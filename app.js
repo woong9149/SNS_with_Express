28cc1e1eb1728ada976f07581bdcd1a02e4f73f0
woong9149 <sewoong0409@naver.com>
2020-10-06 23:49:10 +0900

    프론트 추가, 백엔드 로직 시작
    프론트 추가, 백엔드 로직 시작

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
 const session = require('express-session');
-const flash = require('flash');
-const conf = require('./appconf.json');
+const flash = require('connect-flash');
+require('dotenv').config();//dotenv를 사용
 
 const app = express();
 
@@ -14,11 +16,11 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(cookieParser(conf.password));
+app.use(cookieParser(process.env.COOCIE_SECRET));
 app.use(session({
     resave : false,
     saveUninitialized : false,
-    secret :conf.password,
+    secret :process.env.COOCIE_SECRET,
     cookie : {
         httpOnly : true,
         secure : false,
@@ -26,8 +28,8 @@ app.use(session({
 }));
 app.use(flash());
 
-app.listen(8001, () => {
-    console.log("8001번 포트에서 서버 실행중입니다.");
+app.listen(app.get('port'), () => {//app.set 한 것을 app.get으로 가져올 수 있음
+    console.log(`${app.get('port')}번 포트에서 서버 실행중입니다.`);
 })
 
 
