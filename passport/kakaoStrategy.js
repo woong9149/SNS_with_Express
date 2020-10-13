6bedb5d65f953889d6be2890fdab108aa11d2225
woong9149 <sewoong0409@naver.com>
2020-10-13 12:33:45 +0900

    카카오 로그인 완료
    카카오 로그인 완료

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -4,20 +4,21 @@ const { User } = require('../models');
 module.exports = (passport) => {
     passport.use(new kakaoStrategy({
         clientID: process.env.KAKAO_ID,
-        callbackURL : 'http://localhost:8001/auth/kakao/callback',
+        callbackURL : '/auth/kakao/callback',
     }, async(accessToken, refreshToken, profile, done) => {
         console.log('kakao profile', profile);
         try {
-            const exUser = await User.findOne({ where : {snsId: profile, provider: 'kakao'}});
+            const exUser = await User.findOne({ where : {snsId: profile.id, provider: 'kakao'}});
             if(exUser){
                 done(null, exUser);
             }else{
                 const newUser = await User.create({
-                    email: profile._json && profile._json.kaccount_email,
+                    email: profile._json && profile._json.kakao_account_email,
                     nick: profile.displayName,
                     snsId: profile.id,
                     provider : 'kakao',
                 });
+                console.log('newUser: ', newUser);
                 done(null, newUser);
             }
         } catch (error) {
